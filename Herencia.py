8c22c5e7dea14f9b0a4d9e8d704ea53ca26a10e2
Simon <simon.palominos@protonmail.com>
2023-08-21 18:55:28 -0400

    Se agrega herencia multiple

--- Herencia.py
+++ Herencia.py
@@ -20,3 +20,22 @@ class Estudiante(Persona):
         self.notas = notas
         self.universidad = universidad
                         
+                        
+class Artista:
+    def __init__(self,habilidad):
+        self.habilidad = habilidad
+        
+        
+        def mostrar_habilidad(self):
+            print(f"mi habilidad es: {self.habilidad}")                       
+            
+# Esta es una class que hereda de 2 clases, es una herencia multiple.
+class EmpleadoArtista(Persona,Artista):
+    def __init__(self,nombre,edad,nacionalidad,habilidad,salario,empresa):
+        Persona.__init__(self,nombre,edad,nacionalidad)
+        Artista.__init__(self,habilidad)
+        self.salario = salario
+        self.empresa = empresa
+        
+        def presentar(self):
+            return (f'{self.mostrar_habilidad()}')
